85801c476966d96402293ddbbe5fb192e79712ee
richard <richardsunday0812@gmail.com>
2026-03-06 13:40:36 +0100

    Refactor App component to use Signup instead of StateLogin; enhance Signup form with validation and required fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Header from './components/Header.jsx';
-import StateLogin from './components/StateLogin.jsx';
 import Signup from './components/Signup.jsx';
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
     <>
       <Header />
       <main>
-        <StateLogin />
+        <Signup />
       </main>
     </>
   );
